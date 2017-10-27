4a602a7a51e9bcc74221207e9afa0ef6fa6c8226
zmt5821 <526686667@qq.com>
2017-10-27 11:19:57 +0800

    some init msg

---
+++ js/lbt.js
@@ -0,0 +1,9 @@
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('.flexslider').flexslider({
+				directionNav: true,
+				pauseOnAction: false,
+				slideshowSpeed: 3000
+			});
+		});
+	</script>
